4ed66ca6a407c01cc8a2694cce3ef78eb9fd7311
TomasLonnblad <tomaslonnblad@gmail.com>
2026-02-19 16:12:58 +0100

    x

--- src/app/(main)/dashboard/gesap/page.tsx
+++ src/app/(main)/dashboard/gesap/page.tsx
@@ -3,7 +3,10 @@ import styles from "./styles.module.css";
 const Home = () => {
   return (
     <div className={styles.hello}>
-      <p>rootLaout with children: React.ReactNode;</p>
+        <h1 className="font-semibold text-2xl">My ghost like to travel</h1>
+      <p className="text-muted-foreground">
+My ghost like to travel so far in the unknown
+My ghost like to travel so deep into your space</p>
     </div>
   );
 };
